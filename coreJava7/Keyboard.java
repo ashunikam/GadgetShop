61cad5bdbbb9f7417e39dd307d046ffb6dfa481a
Ashwini Nikam <ashu.nikam26@gmail.com>
2023-07-20 12:18:07 -0700

    modify

---
+++ coreJava7/Keyboard.java
@@ -0,0 +1,9 @@
+package coreJava7;
+
+public class  Keyboard extends Accessary {
+
+	public Keyboard(double price, String name) {
+		super(price, name);
+		// TODO Auto-generated constructor stub
+	}
+}
